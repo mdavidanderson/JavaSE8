cf8bc4cce52317e211b3d21901bdd74fea6ef0e1
Michael Anderson <mdavidanderson@gmail.com>
2015-10-12 15:36:47 -0500

    Adding Chapter14

---
+++ Chapter14/StringConcatenation/src/StringConcatenation.java
@@ -0,0 +1,11 @@
+public class StringConcatenation {
+	public static void main( String[] args ) {
+		String s1 = "Happy ";
+		String s2 = "Birthday";
+		
+		System.out.printf("s1 = %s%ns2 = %s%n%n", s1, s2);
+		System.out.printf("Result of s1.concat(s2) = %s%n", s1.concat(s2));
+		System.out.printf("s1 after s1.concat(s2) = %s%n", s1);
+		
+	}
+}
